7e889150e3fcfda53aa63b5b23dcff45ccc92b62
Tom Reifenberg <62250699+tomreifenberg@users.noreply.github.com>
2020-07-20 22:33:33 -0500

    Update script.js
    Added documentation needed to explain js logic and actions.

--- Bigfoot_JS/script.js
+++ Bigfoot_JS/script.js
@@ -1,7 +1,10 @@
+*/ Ensures Bigfoot loads with image, not before background loads. This would make the game too easy, of course. */
 function loadBigFoot() {
   document.getElementById('bigFoot').src = 'bigfoot.png'
 }
 
+
+*/ Selecting Bigfoot generates victory alert. Bigfoot's location on the page is then shifted from the default position to a randomly revised x - y coordinate for the next round. */
 function moveBigFoot() {
   alert('Woohoo, you win! You found Bigfoot!')
   var picture = document.getElementById('bigFoot')
